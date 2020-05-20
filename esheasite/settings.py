be9b9f4159ef0f9a3d97000f5f770d9457961f83
Elizabeth H Shea <liz.h.shea@gmail.com>
2020-05-20 10:11:39 -0400

    adjusting the settings for prod

--- esheasite/settings.py
+++ esheasite/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4jc0taa065zcml1mc*78@wy0w0qp4b*(50l4og$j!s)62g_w@6'
-
+# SECRET_KEY = '4jc0taa065zcml1mc*78@wy0w0qp4b*(50l4og$j!s)62g_w@6'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '4jc0taa065zcml1mc*78@wy0w0qp4b*(50l4og$j!s)62g_w@6')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ehshea.net', '127.0.0.1']
 
 # Application definition
 
