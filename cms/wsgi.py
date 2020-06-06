4afb8aab2e5030afac1d9e5d84f1381bd31d0356
Elizabeth H Shea <liz.h.shea@gmail.com>
2020-06-06 14:03:03 -0400

    matching the file structure to gandis desired structure

--- cms/wsgi.py
+++ cms/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for esheasite project.
+WSGI config for cms project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -7,9 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
+import sys
 import os
+import os.path
 
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'cms')))
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cms.settings')
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'esheasite.settings')
 application = get_wsgi_application()
